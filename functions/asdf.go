39ec68e2acae311cf7d28852a1e7acf13220e8c4
tafseer-khan <taf@taf.com>
2023-02-01 01:02:39 -0600

    added real code

--- functions/asdf.go
+++ functions/asdf.go
@@ -1,1 +1,28 @@
-asdfasdf
+package lib
+
+import (
+	"github.com/taubyte/go-sdk/event"
+	"github.com/taubyte/go-sdk/pubsub"
+)
+
+//export getsocketurl
+func getsocketurl(e event.Event) uint32 {
+	h, err := e.HTTP()
+	if err != nil {
+		return 1
+	}
+
+	channel, err := pubsub.Channel("channelToMake")
+	if err != nil {
+		return 1
+	}
+
+	url, err := channel.WebSocket().Url()
+	if err != nil {
+		return 1
+	}
+
+	h.Write([]byte(url.Path))
+
+	return 0
+}
